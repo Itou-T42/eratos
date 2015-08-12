31d5e60f9a6ea0a727f83253425efb5396142da5
Itou-T42 <Itou-T42@mail.dnp.co.jp>
2015-08-12 23:22:16 +0900

    素数算出の機能実装を完了

--- eratos.rb
+++ eratos.rb
@@ -1,5 +1,34 @@
-a = ARGV[0].to_i
+x = ARGV[0].to_i
 
-sqr = Math.sqrt(a)
+class Eratos
+  def self.prime(x)
 
-p sqr
+    sqr = Math.sqrt(x)
+
+    serch = Array.new #探索リスト
+    prime = Array.new #素数リスト
+
+    for num in 2..x do
+      serch << num
+    end
+
+    while serch[0].to_f < sqr do
+      start = serch.shift
+      prime << start
+      num = serch.size
+      num -= 1
+
+      for i in 0..num do
+        if serch[i].to_i % start.to_i == 0
+          serch.delete_at(i)
+        end
+      end
+    end
+    
+    prime = prime + serch
+    return prime
+  end
+end
+
+result = Eratos.prime(x)
+p result
